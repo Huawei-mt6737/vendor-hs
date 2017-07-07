04cf6d3c0162781d9a6730f1314f4018b970452a
mdeejay <mdjrussia@gmail.com>
2017-07-08 03:26:33 +0700

    [2/2] Build some MTK stuff from source

--- razar/Android.mk
+++ razar/Android.mk
@@ -12,3 +12,25 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgralloc_extra
+LOCAL_MODULE_OWNER := MediaTek
+LOCAL_SRC_FILES_64 := proprietary/lib64/libgralloc_extra.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libgralloc_extra.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnvram
+LOCAL_MODULE_OWNER := MediaTek
+LOCAL_SRC_FILES_64 := proprietary/lib64/libnvram.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libnvram.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
